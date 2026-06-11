a05c78b5f86695e4b98a35f2890fb983d7b757f5
wixy122018-byte <wixy122018@gmail.com>
2026-06-11 01:57:59 -0500

    Extraída únicamente la carpeta de código desde la rama de Yandry

---
+++ GO_CODE_GESTION/internal/models/user.go
@@ -0,0 +1,12 @@
+package models
+
+import "time"
+
+type User struct {
+	ID        uint      `json:"id" gorm:"primaryKey"`
+	Name      string    `json:"name"`
+	Email     string    `json:"email" gorm:"unique"`
+	Password  string    `json:"-"`
+	Role      string    `json:"role"`
+	CreatedAt time.Time `json:"created_at"`
+}
